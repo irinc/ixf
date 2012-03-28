4748264dd28b63a718b210a4ae510ce3941bb94d
Jason LuBean <jason.lubean@gmail.com>
2012-03-28 12:47:13 -0600

    Added Buster.JS test for Autocomplete.

--- test/buster.js
+++ test/buster.js
@@ -1,27 +1,13 @@
 var config = module.exports;
 
-config["Node Tests"] = {
-	environment: "node",
-	rootPath: "../",
-	libs: ["js/lib/require.js"],
-	tests: [
-		"test/specs/node/*.js"
-	],
-	sources: [
-	],
-	resources: [
-	]
-}
-
 config["Browser Tests"] = {
 	environment: "browser",
 	rootPath: "../",
 	libs: [
 		"build/scripts/3plib/jquery.js",
 		"build/scripts/3plib/jquery-ui.js",
-		"build/scripts/3plib/jquery-ui.js",
 		"ixf/scripts/ixf-plugins.js",
-		"ixf/scripts/ixf-utilities.js"
+		"build/scripts/ixf-utilities.js"
 	],
 	tests: [
 		"test/specs/browser/*.js"
