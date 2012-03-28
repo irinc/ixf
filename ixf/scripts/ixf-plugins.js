69fa5d1a22c405e64a5eed60f93f30a4bd62764b
Jason LuBean <jason.lubean@gmail.com>
2012-03-28 14:58:33 -0600

    Fixed problem with IE 8 not having a String.trim() function

--- ixf/scripts/ixf-plugins.js
+++ ixf/scripts/ixf-plugins.js
@@ -1,7 +1,7 @@
 /*!
  * IxF Plugins 
  * @description This file is a collection of jquery plugins and modernizr used by IxF.
- * @version     1.1.2 - 2012/3/27
+ * @version     1.1.2 - 2012/3/28
  * @copyright   Copyright © 2012 by Intellectual Reserve, Inc.
  * @URL         http://irinc.github.com/ixf
  * global       ixf, $, window
